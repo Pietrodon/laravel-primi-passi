67f9e415e8fb413e0bb044fb0c20a6432da35a13
Pietrodon <donvitopietro00@gmail.com>
2022-04-19 16:14:15 +0200

    creazione homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h2>ciao a tutti</h2>
+        
 
     </body>
 </html>
